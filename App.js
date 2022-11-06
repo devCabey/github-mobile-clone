c22790974f0910e418563ceb95c44f7b924cd0f5
CalebOkyere <agyakojo4321@gmail.com>
2022-11-06 15:34:14 +0000

    native stack done

--- App.js
+++ App.js
@@ -1,24 +1,92 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { TailwindProvider } from "tailwindcss-react-native";
+import HomeStack from "./navigation/HomeStack";
+import Notification from "./screens/Notification";
+import {
+  FontAwesome5,
+  Ionicons,
+  MaterialIcons,
+  Octicons,
+} from "@expo/vector-icons";
+import Explore from "./screens/Explore";
+import Profile from "./screens/Profile";
 
 // Screens
-import HomeScreen from "./screens/HomeScreen";
-import AddHeaderButton from "./components/AddHeaderButton";
 
-const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <TailwindProvider>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen
-            name="Home"
-            component={HomeScreen}
-            options={{ headerRight: () => <AddHeaderButton /> }}
+        <Tab.Navigator
+          screenOptions={{
+            headerShown: false,
+            tabBarActiveTintColor: "#0561f5",
+            tabBarInActiveTintColor: "#a2a2a3",
+          }}
+        >
+          <Tab.Screen
+            component={HomeStack}
+            name="HomeStack"
+            options={{
+              title: "Home",
+              tabBarIcon: ({ focused, color, size }) =>
+                focused ? (
+                  <Ionicons name="md-home-sharp" size={size} color={color} />
+                ) : (
+                  <Ionicons name="md-home-outline" size={size} color={color} />
+                ),
+            }}
           />
-        </Stack.Navigator>
+          <Tab.Screen
+            component={Notification}
+            name="Notification"
+            options={{
+              tabBarIcon: ({ focused, color, size }) =>
+                focused ? (
+                  <Ionicons
+                    name="notifications-sharp"
+                    size={size}
+                    color={color}
+                  />
+                ) : (
+                  <Ionicons
+                    name="notifications-outline"
+                    size={size}
+                    color={color}
+                  />
+                ),
+            }}
+          />
+
+          <Tab.Screen
+            component={Explore}
+            name="Explore"
+            options={{
+              tabBarIcon: ({ focused, color, size }) =>
+                focused ? (
+                  <Octicons name="telescope-fill" size={size} color={color} />
+                ) : (
+                  <Octicons name="telescope" size={size} color={color} />
+                ),
+            }}
+          />
+
+          <Tab.Screen
+            component={Profile}
+            name="Profile"
+            options={{
+              tabBarIcon: ({ focused, color, size }) =>
+                focused ? (
+                  <FontAwesome5 name="user-alt" size={size} color={color} />
+                ) : (
+                  <FontAwesome5 name="user" size={size} color={color} />
+                ),
+            }}
+          />
+        </Tab.Navigator>
       </NavigationContainer>
     </TailwindProvider>
   );
